b3dc8a8a1ab1d922a6ffbd2f40711d9e02c263a8
Zakiul Bakshi <zakiul@outlook.com>
2018-01-02 02:14:36 -0600

    core library added

---
+++ Core/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
